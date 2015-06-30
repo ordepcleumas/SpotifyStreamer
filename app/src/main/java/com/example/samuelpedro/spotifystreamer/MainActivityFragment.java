dcbe7ded29d33feb2b65e0409271f9324e3cf8f6
Samuel Pedro <samuelcpedro@gmail.com>
2015-06-30 22:19:43 +0000

    Já recebo o texto introduzido

--- app/src/main/java/com/example/samuelpedro/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/example/samuelpedro/spotifystreamer/MainActivityFragment.java
@@ -3,10 +3,13 @@ package com.example.samuelpedro.spotifystreamer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,6 +26,7 @@ import kaaes.spotify.webapi.android.models.ArtistsPager;
  */
 public class MainActivityFragment extends Fragment {
 
+    private final String LOG_TAG = MainActivityFragment.class.getSimpleName();
     public BandsAdapter bandAdapter;
     public List listBand;
 
@@ -34,27 +38,44 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         try {
-            //Begin Task
-            FetchArtistTask artistTask = new FetchArtistTask();
-            artistTask.execute("Muse");
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            listBand = new ArrayList<Band>();
-            //listBand.add(new Band(1, "nome1"));
-            //listBand.add(new Band(2, "nome2"));
-            //listBand.add(new Band(3, "nome3"));
-            //listBand.add(new Band(4, "nome4"));
+            EditText editText = (EditText) rootView.findViewById(R.id.fragment_main_editText_id);
+            editText.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if (s.length() != 0) {
+                        //Begin Task
+                        FetchArtistTask artistTask = new FetchArtistTask();
+                        artistTask.execute(s.toString());
+                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+
+            });
+
+            listBand = new ArrayList<>();
 
             bandAdapter = new BandsAdapter(getActivity(), listBand);
-//
+
             ListView listView = (ListView) rootView.findViewById(R.id.fragment_main_listView_id);
             listView.setAdapter(bandAdapter);
 
             return rootView;
 
         } catch (Exception e) {
-            Log.e("asdasd", "Porquê? " + e.getMessage() + " ******" + e.toString() + " ******" + e.getLocalizedMessage());
+            Log.e(LOG_TAG, "Error ", e);
             return null;
         }
     }
@@ -93,6 +114,7 @@ public class MainActivityFragment extends Fragment {
         @Override
         protected void onPostExecute(List artistList) {
             super.onPostExecute(artistList);
+
             if (!artistList.isEmpty()) {
                 bandAdapter.clear();
                 for (int i = 0; i < artistList.size(); i++) {
